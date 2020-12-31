3645277e7fdeb1eb375ae796d14d9afbb3f3f43c
Dominick Cardone <cardoned11@gmail.com>
2020-12-31 00:24:45 -0500

    added rules page, load screen, feature template

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,26 @@
 function joinDiscord() {
   window.open("https://discord.gg/Krkb42XYjH");
 }
+
+var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
